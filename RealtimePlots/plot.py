b5618bea4ebc28ebeb89f4795f1245b0c6da64e8
Muftawo <hajjmuf@gmail.com>
2022-06-02 14:12:39 +0000

    plot realtime

--- RealtimePlots/plot.py
+++ RealtimePlots/plot.py
@@ -3,13 +3,14 @@ from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.ticker as mtick
 
 plt.style.use('fivethirtyeight')
 
-x_vals = []
-y_vals = []
+# x_vals = []
+# y_vals = []
 
-index = count()
+# index = count()
 
 
 def animate(i):
@@ -18,15 +19,20 @@ def animate(i):
     current_time = data['time']
     value = data['OOB']
 
-    plt.cla()
+    # plt.cla()
+    ax = data["OOB"].plot()
 
-    plt.plot( current_time,value)
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 
+    # ax=plt.plot(current_time,value)
+    # print(type(ax))
+    # print(ax)
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
     # plt.legend(loc='upper right')
     plt.tight_layout()
 
 
-ani = FuncAnimation(plt.gcf(), animate, interval=10000)
+ani = FuncAnimation(plt.gcf(), animate,frames = 200, interval=10000, blit = True)
 
 plt.tight_layout()
 plt.show()
