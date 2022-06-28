ac5ff264924d99c621713c36754157893c145e68
Muftawo <hajjmuf@gmail.com>
2022-06-28 17:13:10 +0000

    refactored realtime plot

--- RealtimePlots/plot.py
+++ RealtimePlots/plot.py
@@ -11,28 +11,45 @@ plt.style.use('fivethirtyeight')
 # y_vals = []
 
 # index = count()
-
+fig= plt.figure()
+ax1=fig.add_subplot(1,1,1)
+ax1.grid(False)
+# ax1.set_ylim(0,20)
 
 def animate(i):
+    leng=200
     data = pd.read_csv('data.csv')
     # x = data['idx']
-    current_time = data['time']
-    value = data['OOB']
-
+    current_time = data['time'].to_list()[-leng:]
+    value = data['OOB'].to_list()[-leng:]
+    # print(current_time)
     # plt.cla()
-    ax = data["OOB"].plot()
+    # ax = data["OOB"].plot()
 
-    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    # ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 
     # ax=plt.plot(current_time,value)
     # print(type(ax))
     # print(ax)
-    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    # ax.yaxis.set_major_formatter(mtick.PercentFormatter())
     # plt.legend(loc='upper right')
-    plt.tight_layout()
+    # plt.tight_layout()
+
+    ax1.clear()
+    ax1.grid(False)
+    ax1.set_ylim(0,max(value)+5)
+    ax1.plot(current_time,value)
+    ax1.fill_between([13]*leng,[11]*leng,[15]*leng,alpha=0.2)
+    plt.axhline(y=13, color='r', linestyle='--')
+    plt.gca().axes.xaxis.set_ticklabels([])
+    plt.gcf().autofmt_xdate()
 
 
-ani = FuncAnimation(plt.gcf(), animate,frames = 200, interval=10000, blit = True)
+    
 
-plt.tight_layout()
+
+
+ani = FuncAnimation(fig, animate, interval=100)
+
+# plt.tight_layout()
 plt.show()
